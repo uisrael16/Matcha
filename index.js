951b8eaff7c39e3fa1a670b123baa4f8de848651
Unathi Nkomo <50695696+uisrael16@users.noreply.github.com>
2020-01-06 16:40:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,18 @@
  const PORT = 3000;
  const app = express();
 
-//  Declare Routers
- var indexRouter = require("./router/index");
-
 // Set View Engine
  app.set("view engine", "ejs");
 
-//  Using and linking Router
- app.use("/", indexRouter);
+//  Using and linking Router to move around the pages
+ 
+ app.get('/Login', (req, res) =>{
+     res.render('Login.ejs')
+ })
 
+ app.get('/Register', (req, res) =>{
+    res.render('Register.ejs')
+})
  app.listen(PORT, () => { 
      console.log(`Server is listening on port: ${PORT}`);  
  });
