5fe9a0ab39a2aa3de6c203902611192f763095c0
Unathi Nkomo <50695696+uisrael16@users.noreply.github.com>
2020-01-07 18:07:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
  const express = require("express");
  const PORT = 3000;
  const app = express();
+ const mysql = require('mysql');
+
+ // Create connection
+ const db = mysql.createConnection({
+     host       : 'localhost',
+     user       : 'root',
+     port       : '3000',
+     password   : '123456789',
+     socketpath : '/goinfre/uisrael/Desktop/mamp/mysql/tmp/mysql.sock',
+     database   : 'Matcha'
+ });
+
+ // Connect
+ db.connect((err) => {
+     if (err){
+         throw err;
+     }
+     console.log('MySql Connected...');
+ });
+
+ // Create DB
+
+app.get('/crealedb', (req, res) => {
+    let sql = 'CREATE DATABASE Matcha';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('database created...');
+    });
+});
 
 // Set View Engine
  app.set("view engine", "ejs");
